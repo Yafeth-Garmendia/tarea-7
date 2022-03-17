34763743cc2c4c499664b514f98d404a5039935f
Yafeth-Garmendia <98914311+Yafeth-Garmendia@users.noreply.github.com>
2022-03-16 21:34:52 -0600

    Add files via upload

---
+++ tarea 7/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+ void menu();
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char const *argv[]) 
+{
+	menu();
+ 
+	return 0;
+}
